4822158871183966736f107c97180cc81fbee91e
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-09-07 10:52:26 +0300

    add scroll to top

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from "react-scroll-to-top";
 import { App } from 'components/App';
 import './index.css';
 import { GlobalStyle } from 'components/GlobalStyle';
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='/goit-react-hw-05-movies'>
       <App />
+      <ScrollToTop smooth />
       <GlobalStyle/>
     </BrowserRouter>
   </React.StrictMode>
